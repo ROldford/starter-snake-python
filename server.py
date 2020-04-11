c7f1ce373a0315e4c7186aaffa454cd4f508ad84
Ryan Oldford <ryan.oldford@gmail.com>
2020-04-11 16:13:37 -0700

    Testing new move code

--- server.py
+++ server.py
@@ -42,7 +42,8 @@ class Battlesnake(object):
 
         # Choose a random direction to move in
         possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
+        # move = random.choice(possible_moves)
+        move = "right"
 
         print(f"MOVE: {move}")
         return {"move": move}
