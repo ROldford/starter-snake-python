5246a2c46d217026841662770fd67a739f5a8922
Ryan Oldford <ryan.oldford@gmail.com>
2020-04-11 18:33:50 -0700

    Bugfix + code cleanup

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 import os
-import random
 
 import cherrypy
 
 """
 This is a simple Battlesnake server written in Python.
-For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
+For instructions see 
+https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
 """
 
 
@@ -17,7 +17,8 @@ class Battlesnake(object):
 
     @cherrypy.expose
     def ping(self):
-        # The Battlesnake engine calls this function to make sure your snake is working.
+        # The Battlesnake engine calls this function
+        # to make sure your snake is working.
         return "pong"
 
     @cherrypy.expose
@@ -25,9 +26,11 @@ class Battlesnake(object):
     @cherrypy.tools.json_out()
     def start(self):
         # This function is called everytime your snake is entered into a game.
-        # cherrypy.request.json contains information about the game that's about to be played.
-        # TODO: Use this function to decide how your snake is going to look on the board.
-        data = cherrypy.request.json
+        # cherrypy.request.json contains information about the game
+        # that's about to be played.
+        # TODO: Use this function to decide
+        #       how your snake is going to look on the board.
+        # data = cherrypy.request.json
         print("START")
         return {"color": "#61abff", "headType": "silly", "tailType": "sharp"}
 
@@ -35,20 +38,22 @@ class Battlesnake(object):
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def move(self):
-        # This function is called on every turn of a game. It's how your snake decides where to move.
+        # This function is called on every turn of a game. It's how your
+        # snake decides where to move.
         # Valid moves are "up", "down", "left", or "right".
-        # TODO: Use the information in cherrypy.request.json to decide your next move.
+        # TODO: Use the information in cherrypy.request.json to
+        #       decide your next move.
         data = cherrypy.request.json
 
         # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
+        # possible_moves = ["up", "down", "left", "right"]
         # move = random.choice(possible_moves)
 
         head = data["you"]["body"][0]
         print(head)
 
         move = "right"
-        if (head["x"] = 10) :
+        if head["x"] == 10:
             move = "up"
 
         print(f"MOVE: {move}")
@@ -58,8 +63,9 @@ class Battlesnake(object):
     @cherrypy.tools.json_in()
     def end(self):
         # This function is called when a game your snake was in ends.
-        # It's purely for informational purposes, you don't have to make any decisions here.
-        data = cherrypy.request.json
+        # It's purely for informational purposes,
+        # you don't have to make any decisions here.
+        # data = cherrypy.request.json
         print("END")
         return "ok"
 
@@ -68,7 +74,7 @@ if __name__ == "__main__":
     server = Battlesnake()
     cherrypy.config.update({"server.socket_host": "0.0.0.0"})
     cherrypy.config.update(
-        {"server.socket_port": int(os.environ.get("PORT", "8080")),}
+        {"server.socket_port": int(os.environ.get("PORT", "8080")), }
     )
     print("Starting Battlesnake Server...")
     cherrypy.quickstart(server)
